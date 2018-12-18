c0ff6bcbed0105e1fe6079c5261b647f476205eb
bazzie <bas@wildemann.nl>
2018-12-18 16:36:14 +0100

    error handling

--- main.go
+++ main.go
@@ -121,13 +121,13 @@ func setGitTag(d string, ver string) string {
 
 	cmdTag := exec.Command("git", "tag", "-a", ver, "-m", "\"gitlab ci tag\"")
 	cmdTag.Dir = d
-	out, err := cmdTag.Output()
+	_, err := cmdTag.Output()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 
-	/*
+
 	cmdPushTag := exec.Command("git", "push", "--tags")
 	cmdPushTag.Dir = d
 	outPushTag, err := cmdPushTag.Output()
@@ -135,7 +135,7 @@ func setGitTag(d string, ver string) string {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-	*/
 
-	return string(out)
+
+	return string(outPushTag)
 }
